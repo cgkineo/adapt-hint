766ff0627de6bd7a7adbf1b6fdb670d7c1e685a5
kirsty-hames <kirstyhames@kirsty.local>
2015-01-28 15:36:19 +0000

    inital commit

---
+++ README.md
@@ -0,0 +1,22 @@
+adapt-component-spec
+====================
+
+display component specification
+
+
+Exmaple JSON
+
+```
+"_componentSpec": {
+    "_browserSupport": {
+        "title": "Browser Support",
+        "minimumSupport": "IE8+",
+        "notes": "Browser Support Notes"
+    },
+    "_accessibilitySupport": {
+        "title": "Accessibility Support",
+        "supported": "AA",
+        "notes": "Accessibility notes"
+    }
+}
+```
---
+++ js/adapt-component-spec.js
@@ -0,0 +1,92 @@
+/*
+* adapt-component-spec
+* Version - 0.0.0
+* License - http://github.com/adaptlearning/adapt_framework/LICENSE
+* Maintainers - Kirsty Hames <kirsty.hames@kineo.com>
+*/
+define(function(require) {
+
+	var Adapt = require('coreJS/adapt');
+	var Backbone = require('backbone');
+
+	var ComponentSpecView = Backbone.View.extend({
+
+		events: {
+			"click .component-spec-show-detail-button":"onSpecButtonClicked"
+		},
+		
+		className: 'component-spec',
+
+		initialize: function() {
+			this.preRender();
+			this.render();
+		},
+
+		preRender: function() {
+
+		},
+
+		render: function() {
+			var data = this.model.toJSON();
+			var template = Handlebars.templates['component-spec'];
+			this.$el.html(template(data)).appendTo($('.' + this.model.get('_id')));
+			_.defer(_.bind(this.postRender, this));
+		},
+
+		postRender: function() {
+			this.setLayout();
+		},
+
+		setLayout: function() {
+			$('.' + this.model.get('_id')).css({
+				position: 'relative'
+			});
+			$('.' + this.model.get('_id') + " .component-title-inner").css({
+				paddingRight: '40px'
+			});
+			var $specDetail = this.$('.component-spec-detail');
+			$specDetail.velocity({
+				scaleX: 0,
+				scaleY: 0
+			}, {
+				duration: 1
+			});
+		},
+
+		onSpecButtonClicked: function(event) {
+			if (event) event.preventDefault();
+			var $specDetail = this.$('.component-spec-detail');
+			if (!$specDetail.hasClass('detail-open')) {
+				$specDetail.velocity({
+					scaleX: 1,
+					scaleY: 1
+				}, {
+					duration: 800,
+					display: 'block',
+					easing: [500, 35]
+				});
+				$specDetail.addClass('detail-open');
+				this.$('.component-spec-show-detail-button').removeClass('icon-question').addClass('icon-cross');
+			} else {
+				$specDetail.velocity({
+					scaleX: 0,
+					scaleY: 0
+				}, {
+					duration: 300,
+					display: 'none'
+				});
+				$specDetail.removeClass('detail-open');
+				this.$('.component-spec-show-detail-button').removeClass('icon-cross').addClass('icon-question');
+			}
+		}
+	});
+
+	Adapt.on('componentView:postRender', function(view) {
+		if (view.model.get('_componentSpec')) {
+			new ComponentSpecView({
+				model: view.model
+			});
+		}
+	});
+
+});
