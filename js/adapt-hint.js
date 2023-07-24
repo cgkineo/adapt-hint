4c51c644d10aaf5de90e24fc1b8a614f2d22ec0e
kirsty-hames <kirsty-hames@users.noreply.github.com>
2023-07-24 13:27:36 +0100

    remove native popup and use notify popup only
    - _isNotifyPopup config removed and notify made default
    - closeButtonText removed as this was only applicable to the native popup

--- js/adapt-hint.js
+++ js/adapt-hint.js
@@ -22,7 +22,6 @@ define([
   const HintView = Backbone.View.extend({
 
     events: {
-      'click .js-hint-btn-toggle': 'onHintToggleClicked',
       'click .js-hint-btn-popup': 'onHintPopupClicked'
     },
 
@@ -30,11 +29,6 @@ define([
 
     initialize: function() {
       this.listenTo(Adapt, 'remove', this.remove);
-
-      if (!this.model.get('_hint')._isNotifyPopup) {
-        this.listenTo(Adapt, 'hint:opened', this.checkIfShouldClose);
-      }
-
       this.render();
     },
 
@@ -58,41 +52,6 @@ define([
         _showCloseButton: true,
         _closeOnBackdrop: true
       });
-    },
-
-    onHintToggleClicked: function (e) {
-      const $button = this.$('.js-hint-btn-toggle');
-      const $widget = this.$('.js-hint-widget');
-      const globals = Adapt.course.get('_globals')._extensions._hint;
-      const isBeingOpened = $widget.hasClass('is-closed');
-
-      $widget
-        .toggleClass('is-open', isBeingOpened)
-        .toggleClass('is-closed', !isBeingOpened);
-      $button
-        .toggleClass('is-open', isBeingOpened)
-        .toggleClass('is-closed', !isBeingOpened)
-        .attr('aria-label', (isBeingOpened ? globals.closeButtonText : globals.openButtonText));
-
-      if (isBeingOpened) {
-        Adapt.a11y.popupOpened($widget);
-        Adapt.trigger('hint:opened', this.model.get('_id'));
-        return;
-      }
-
-      Adapt.a11y.popupClosed($button);
-    },
-
-    /**
-     * Handles closing the hint pop-out if another hint was opened
-     * @param {string} id The component id of the hint that triggered this event
-     */
-    checkIfShouldClose: function (id) {
-      if (this.model.get('_id') === id) return;
-
-      if (this.$('.js-hint-btn-toggle').hasClass('is-closed')) return;
-
-      this.onHintToggleClicked();
     }
 
   });
